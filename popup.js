31d31a368ddb51a41dcbbf08194adb88452d4a62
Naoki Sekiguchi <sekiguchi@revcomm.co.jp>
2022-11-30 22:21:41 +0900

    Add constants.js

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
+import { HELLO } from './constants.js';
+
 const $body = document.querySelector('body');
 const $p = document.createElement('p');
-$p.innerHTML = 'Hello Popup';
+$p.innerHTML = `${HELLO} popup`;
 if ($body) {
   $body.appendChild($p);
 }
